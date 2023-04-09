841447e23801e2757838b942441d46b697a6f63b
pbullock08 <126013657+pbullock08@users.noreply.github.com>
2023-04-09 13:26:19 -0400

    added content to HTML for first two pages and started working on question boxes in javascript

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+var question1 = [
+    {
+        name: "Answer 1"
+    },
+    {
+        name: "Answer 2"
+    },
+    {
+        name: "Answer 3"
+    },
+    {
+        name: "Answer 4"
+    },
+];
+
+for (var i=0; i < question1.length; i++) {
+    console.log(question1[i]);
+}
