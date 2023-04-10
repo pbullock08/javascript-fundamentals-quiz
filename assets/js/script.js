6622cf426d70d1b3bf67e16894006fbdfca5bc94
pbullock08 <126013657+pbullock08@users.noreply.github.com>
2023-04-09 21:20:09 -0400

    added comments all the steps needed to complete for HTML, CSS, and JavaScript files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,21 @@
+//add eventlistener to start quiz and timer while hiding first question
+
+//unhide first question 
+
+//add additional question and answers 
+
+var question = document.querySelector(".question-and-answers");
+
+question.addEventListener("click", changeQuestion);
+
+function changeQuestion() {
+    question.children[0].textContent = "The condition in an if/else statement is enclosed with ________.";
+    question.children[1].children[0].textContent = "quotes";
+    question.children[1].children[1].textContent = "curly brackets";
+    question.children[1].children[2].textContent = "parenthesis";
+    question.children[1].children[3].textContent = "square brackets";
+}
+
 var question1 = [
     {
         name: "Answer 1"
@@ -16,3 +34,25 @@ var question1 = [
 for (var i=0; i < question1.length; i++) {
     console.log(question1[i]);
 }
+
+
+
+//create answer key 
+
+//for answer key 
+//if a1 === && a2 === {
+    //correct 
+    // move to next question 
+//} else {
+    //wrong 
+    //move to next question
+    //subtract 10 seconds from time 
+//}
+
+//log score to local storage 
+
+//display local storage of high scores in order of  
+
+
+
+
