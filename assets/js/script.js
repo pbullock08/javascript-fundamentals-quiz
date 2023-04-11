0082c542f9dc7425a63fe49e84cdad4f5f394763
pbullock08 <126013657+pbullock08@users.noreply.github.com>
2023-04-11 13:46:22 -0400

    fixed issue with questions not operating on a for loop

--- assets/js/script.js
+++ assets/js/script.js
@@ -25,6 +25,8 @@ function setTime() {
 };
 
 //add additional question and answers 
+var boxEl = document.querySelector(".quiz");
+
 var quizKey = [
         {
             question: "Commonly used data types DO NOT include:",
@@ -72,20 +74,19 @@ var quizKey = [
         }
     ];
     
-    for (var i=0; i < quizKey; i++) {
+    for (var i = 0; i < quizKey; i++) {
         console.log(quizKey[i]);
     }
-
-    var boxEl = document.querySelector(".quiz");
-    boxEl.addEventListener("click", switchContent);
     
+    var questionIndex = 1;
+
+    boxEl.addEventListener("click", switchContent);
     function switchContent() {
         var questions = document.querySelector(".question");
         var option1 = document.querySelector("#choice1");
         var option2 = document.querySelector("#choice2");
         var option3 = document.querySelector("#choice3");
         var option4 = document.querySelector("#choice4");
-        var questionIndex = 1;
         
         questions.textContent = quizKey[questionIndex].question;
         option1.textContent = quizKey[questionIndex].choice1;
@@ -94,13 +95,8 @@ var quizKey = [
         option4.textContent = quizKey[questionIndex].choice4;
         console.log(quizKey[questionIndex]);
         questionIndex++;
-        if(questionIndex >= quizKey.length) {
-            questionIndex = 0;
-        }
     }
         
-      
-
 // choices.addEventListener("click", selectAnswer);
 
 // function selectAnswer() {
@@ -122,101 +118,7 @@ var quizKey = [
 // }
 
 
-
-// var quizKey = [
-//     {
-//         question: "Commonly used data types DO NOT include:",
-//         choice1: "strings", 
-//         choice2: "booleans", 
-//         choice3: "alerts", 
-//         choice4: "numbers",
-//         answer: "alerts",
-//     },
-
-//     {
-//         question: "The condition in an if/else statement is enclosed with ________.",
-//         choice1: "quotes", 
-//         choice2: "curly brackets", 
-//         choice3: "parenthesis", 
-//         choice4: "square brackets",
-//         answer: "curly brackets",
-//     },
-
-//     {
-//         question: "Arrays in JavaScript can be used to store ________.",
-//         choice1: "numbers and strings", 
-//         choice2: "other arrays", 
-//         choice3: "booleans", 
-//         choice4: "all f the above",
-//         answer: "all of the above",
-//     },
-
-//     {
-//         question: "String values must be enclosed within ________ when being assigned to values.",
-//         choice1: "commas", 
-//         choice2: "curly brackets", 
-//         choice3: "quotes", 
-//         choice4: "parenthesis",
-//         answer: "quotes",
-//     },
-
-//     {
-//         question: "A very useful tool used during development and debugging for printing content to the debugger is:",
-//         choice1: "JavaScript", 
-//         choice2: "terminal/bash", 
-//         choice3: "for loops", 
-//         choice4: "console log",
-//         answer: "console log",
-//     },
-// ];
-
-// var boxEl = document.querySelector("#content-box");
-// boxEl.addEventListener("click", switchContent);
-
-// function switchContent() {
-//     boxEl.innerHTML = "";
-
-//     var questions = document.createElement("h4");
-//     var optionsEl = document.createElement("ol");
-//     var option1 = document.createElement("li");
-//     var option2 = document.createElement("li");
-//     var option3 = document.createElement("li");
-//     var option4 = document.createElement("li");
-//     var questionIndex = 0;
-
-//     questions.textContent = quizKey[questionIndex].question;
-//     option1.textContent = quizKey[questionIndex].choice1;
-//     option2.textContent = quizKey[questionIndex].choice2;
-//     option3.textContent = quizKey[questionIndex].choice3;
-//     option4.textContent = quizKey[questionIndex].choice4;
-
-//     boxEl.appendChild(questions);
-//     boxEl.appendChild(optionsEl);
-//     boxEl.children[1].appendChild(option1);
-//     boxEl.children[1].appendChild(option2);
-//     boxEl.children[1].appendChild(option3);
-//     boxEl.children[1].appendChild(option4);
-
-//     console.log(quizKey[questionIndex]);
-//     questionIndex++;
-//     if(questionIndex >= quizKey.length) {
-//         questionIndex = 0;
-//     }
-// };
-
-
-
 //create answer key 
-
-//for answer key 
-//if a1 === && a2 === {
-    //correct 
-    // move to next question 
-//} else {
-    //wrong 
-    //move to next question
-    //timeLeft = timeLeft - 10
-//}
 
 //for last question whether wrong or right when you click on an answer it stops time and then logs that time on page with input 
 
