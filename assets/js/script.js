6357878a1c0ac6a4ad2bfe0b89de3834450749e8
pbullock08 <126013657+pbullock08@users.noreply.github.com>
2023-04-11 20:51:05 -0400

    added event listeners on each choice so that i can assess what to do if the choice clicked matches the question answer

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,6 +23,7 @@ function setTime() {
     coverPage.innerHTML = "";
 
     quiz.setAttribute ("style", "display:block");
+    switchContent();
 };
 
 var quizKey = [
@@ -32,7 +33,7 @@ var quizKey = [
         choice2: "2. booleans", 
         choice3: "3. alerts", 
         choice4: "4. numbers",
-        answer: "alerts",
+        answer: "3. alerts",
     },
         
     {
@@ -41,7 +42,7 @@ var quizKey = [
         choice2: "2. curly brackets", 
         choice3: "3. parenthesis", 
         choice4: "4. square brackets",
-        answer: "curly brackets",
+        answer: "2. curly brackets",
     },
         
     {
@@ -50,7 +51,7 @@ var quizKey = [
         choice2: "2. other arrays", 
         choice3: "3. booleans", 
         choice4: "4. all f the above",
-        answer: "all of the above",
+        answer: "4. all of the above",
     },
         
     {
@@ -59,7 +60,7 @@ var quizKey = [
         choice2: "2. curly brackets", 
         choice3: "3. quotes", 
         choice4: "4. parenthesis",
-        answer: "quotes",
+        answer: "3. quotes",
     },
         
     {
@@ -68,7 +69,7 @@ var quizKey = [
         choice2: "2. terminal/bash", 
         choice3: "3. for loops", 
         choice4: "4. console log",
-        answer: "console log",
+        answer: "4. console log",
     }
 ];
 
@@ -81,183 +82,93 @@ var option4 = document.querySelector(".choice4");
 var correct = document.querySelector(".correct");
 var wrong = document.querySelector(".wrong");
 
-option3.addEventListener("click", correct1);
-function correct1() {
-    // correct.setAttribute("style", "display:block");
-
-    questions.textContent = quizKey[1].question;
-    option1.textContent = quizKey[1].choice1;
-    option2.textContent = quizKey[1].choice2;
-    option3.textContent = quizKey[1].choice3;
-    option4.textContent = quizKey[1].choice4;
-
-    option1.className = "choice11"
-    option2.className = "choice21"
-    option3.className = "choice31"
-    option4.className = "choice41"
-}
-
-option1.addEventListener("click", wrong1);
-option2.addEventListener("click", wrong1);
-option4.addEventListener("click", wrong1);
-
-function wrong1() {
-    //wrong.setAttribute("style", "display:block");
-    
-    questions.textContent = quizKey[1].question;
-    option1.textContent = quizKey[1].choice1;
-    option2.textContent = quizKey[1].choice2;
-    option3.textContent = quizKey[1].choice3;
-    option4.textContent = quizKey[1].choice4;
-
-    option1.className = "choice11"
-    option2.className = "choice21"
-    option3.className = "choice31"
-    option4.className = "choice41"
-
-    timeLeft -= 10;
-}
-
-var option11 = document.querySelector(".choice11")
-var option21 = document.querySelector(".choice21")
-var option31 = document.querySelector(".choice31")
-var option41 = document.querySelector(".choice41")
-
-option21.addEventListener("click", correct2);
-function correct2() {
-    // correct.setAttribute("style", "display:block");
-    alert ("clicked");
-     
-    questions.textContent = quizKey[2].question;
-    option11.textContent = quizKey[2].choice1;
-    option12.textContent = quizKey[2].choice2;
-    option13.textContent = quizKey[2].choice3;
-    option14.textContent = quizKey[2].choice4;
-
-    option11.className = "choice12"
-    option21.className = "choice22"
-    option31.className = "choice32"
-    option41.className = "choice42"
-}
-
-option11.addEventListener("click", wrong2);
-option31.addEventListener("click", wrong2);
-option41.addEventListener("click", wrong2);
-
-function wrong2() {
-    alert ("clicked");
-    //wrong.setAttribute("style", "display:block");
-    
-    var questionIndex = 2;
-     
-    questions.textContent = quizKey[2].question;
-    option11.textContent = quizKey[2].choice1;
-    option21.textContent = quizKey[2].choice2;
-    option31.textContent = quizKey[2].choice3;
-    option41.textContent = quizKey[2].choice4;
-
-    option11.className = "choice12"
-    option21.className = "choice22"
-    option31.className = "choice32"
-    option41.className = "choice42"
-
-    timeLeft -= 10;
-}
-
-
 //add additional question and answers 
-// var boxEl = document.querySelector("#choices");
+var boxEl = document.querySelector(".quiz");
+var questionIndex = 0;
 
-// var quizKey = [
-//     {
-//         question: "Commonly used data types DO NOT include:",
-//         choice1: "1. strings", 
-//         choice2: "2. booleans", 
-//         choice3: "3. alerts", 
-//         choice4: "4. numbers",
-//         answer: "alerts",
-//     },
-    
-//     {
-//         question: "The condition in an if/else statement is enclosed with ________.",
-//         choice1: "1. quotes", 
-//         choice2: "2. curly brackets", 
-//         choice3: "3. parenthesis", 
-//         choice4: "4. square brackets",
-//         answer: "curly brackets",
-//     },
-    
-//     {
-//         question: "Arrays in JavaScript can be used to store ________.",
-//         choice1: "1. numbers and strings", 
-//         choice2: "2. other arrays", 
-//         choice3: "3. booleans", 
-//         choice4: "4. all f the above",
-//         answer: "all of the above",
-//     },
-    
-//     {
-//         question: "String values must be enclosed within ________ when being assigned to values.",
-//         choice1: "1. commas", 
-//         choice2: "2. curly brackets", 
-//         choice3: "3. quotes", 
-//         choice4: "4. parenthesis",
-//         answer: "quotes",
-//     },
-    
-//     {
-//         question: "A very useful tool used during development and debugging for printing content to the debugger is:",
-//         choice1: "1. JavaScript", 
-//         choice2: "2. terminal/bash", 
-//         choice3: "3. for loops", 
-//         choice4: "4. console log",
-//         answer: "console log",
-//     }
-// ];
+boxEl.addEventListener("click", switchContent);
 
-// var questionIndex = 1;
-
-// boxEl.addEventListener("click", switchContent);
-
-// function switchContent() {
-//     var questions = document.querySelector(".question");
-//     var option1 = document.querySelector("#choice1");
-//     var option2 = document.querySelector("#choice2");
-//     var option3 = document.querySelector("#choice3");
-//     var option4 = document.querySelector("#choice4");
+function switchContent() {
+    var questions = document.querySelector(".question");
+    var option1 = document.querySelector(".choice1");
+    var option2 = document.querySelector(".choice2");
+    var option3 = document.querySelector(".choice3");
+    var option4 = document.querySelector(".choice4");
         
-//     questions.textContent = quizKey[questionIndex].question;
-//     option1.textContent = quizKey[questionIndex].choice1;
-//     option2.textContent = quizKey[questionIndex].choice2;
-//     option3.textContent = quizKey[questionIndex].choice3;
-//     option4.textContent = quizKey[questionIndex].choice4;
-//     questionIndex++;
-// }
+    questions.textContent = quizKey[questionIndex].question;
+    option1.textContent = quizKey[questionIndex].choice1;
+    option2.textContent = quizKey[questionIndex].choice2;
+    option3.textContent = quizKey[questionIndex].choice3;
+    option4.textContent = quizKey[questionIndex].choice4;
+}
 
-// choices.addEventListener("click", selectAnswer);
+option1.addEventListener("click", select1);
+function select1() {
+    if (option1.textContent === quizKey[questionIndex.answer]) {
+        alert ("clicked");
+        correct.setAttribute("style", "display:block");
+        questionIndex++;
 
-// function selectAnswer() {
-//     if (choice1.clicked == true) {
-//         alert("Hello World!")
-//     }
-// }
+    } else {
+        alert ("clicked");
+        wrong.setAttribute("style", "display:block");
+        timeLeft -= 10;
+        questionIndex++;
+    }
+}
 
-// var question = document.querySelector(".question-and-answers");
+option2.addEventListener("click", select2);
+function select2() {
+    if (option2.textContent === quizKey[questionIndex.answer]) {
+        alert ("clicked");
+        correct.setAttribute("style", "display:block");
+        questionIndex++;
+        
+    } else {
+        alert ("clicked");
+        wrong.setAttribute("style", "display:block");
+        timeLeft -= 10;
+        questionIndex++;
+    }
+}
 
-// question.addEventListener("click", changeQuestion);
+option3.addEventListener("click", select3);
+function select3() {
+    if (option3.textContent === quizKey[questionIndex.answer]) {
+        alert ("clicked");
+        correct.setAttribute("style", "display:block");
+        questionIndex++;
+        
+    } else {
+        alert ("clicked");
+        wrong.setAttribute("style", "display:block");
+        timeLeft -= 10;
+        questionIndex++;
+    }
+}
 
-// function changeQuestion() {
-//     question.children[0].textContent = "The condition in an if/else statement is enclosed with ________.";
-//     question.children[1].children[0].textContent = "quotes";
-//     question.children[1].children[1].textContent = "curly brackets";
-//     question.children[1].children[2].textContent = "parenthesis";
-//     question.children[1].children[3].textContent = "square brackets";
-// }
+option4.addEventListener("click", select4);
+function select4() {
+    if (option1.textContent === quizKey[questionIndex.answer]) {
+        alert ("clicked");
+        correct.setAttribute("style", "display:block");
+        questionIndex++;
+        
+    } else {
+        alert ("clicked");
+        wrong.setAttribute("style", "display:block");
+        timeLeft -= 10;
+        questionIndex++;
+    }
+}
+
+function logHighScore() {
+    clearInterval(timerInterval);
+}
 
 
-//create answer key 
 
-//for last question whether wrong or right when you click on an answer it stops time and then logs that time on page with input 
+//for last question when you click on an answer it stops time and then logs that time on page with input 
 
 //log score to local storage after last question or if time = 0
 
